e2ee7d4820ee3af3e40ca69dbd19e7d135fc188c
ying xin <395488553@qq.com>
2019-06-14 13:30:50 +0800

    Update provisionactdispatcher.h

--- message_dispatcher/provisionactdispatcher.h
+++ message_dispatcher/provisionactdispatcher.h
@@ -3,20 +3,72 @@
 #include "msghandler.h"
 
 namespace provision {
+
+enum class ProvisioningType
+{
+    Config,
+    SoftwareUpgrade,
+    SoftwareRollback,
+    UserManagement
+};
+
+inline std::ostream& operator<<(std::ostream& out, ProvisioningType type)
+{
+    switch (type) {
+    case ProvisioningType::Config:
+        return out << "Config";
+    case ProvisioningType::SoftwareUpgrade:
+        return out << "SoftwareUpgrade";
+    case ProvisioningType::SoftwareRollback:
+        return out << "SoftwareRollback";
+    case ProvisioningType::UserManagement:
+        return out << "UserManagement";
+    default:
+        return out << "Invalid";
+    }
+}
+
 template<int MsgId, typename MsgType>
 class ProvisionBase : public msg::TypedMessageHandler<MsgId,MsgType>
 {
 public:
+    using Base = ProvisionBase<MsgId, MsgType>;
+    using HandlerType = msg::TypedMessageHandler<MsgId, MsgType>;
+
     virtual ~ProvisionBase<MsgId,MsgType>()=default;
-    void handleTyped(const MsgType& msg)
-    {}
+    void handleTyped(const MsgType msg) override
+    {
+        std::cout << "ProvisionBase handle Type \n";
+        try{
+
+            handlers[provision::ProvisioningType::Config]->handle(msg);
+        }catch(const std::exception& ex)
+        {
+            std::cout << "could not dispatch mes: " << ex.what() << std::endl;
+        }
+    }
     void addHandler(std::shared_ptr<HandlerType> handler, ProvisioningType type)
-    {}
+    {
+        std::cout << "add handler: " << type << std::endl;
+        handlers[type] = handler;
+    }
+
+private:
+    std::map<ProvisioningType, std::shared_ptr<HandlerType>> handlers;
 };
 
-class ActDispatcher : public ProvisionBase<ACT_MSG,int>
+}
+
+class ActDispatcher : public provision::ProvisionBase<ACT_MSG,int>
 {
 public:
-    ActDispatcher() {}
+    using Base::ProvisionBase;
 };
-}
+
+
+
+class ValDispatcher : public provision::ProvisionBase<VAL_MSG,int>
+{
+public:
+    using Base::ProvisionBase;
+};
